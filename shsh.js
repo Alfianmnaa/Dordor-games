e5e11efbd097169619a3af7dcac3fd6766d5584d
Alfian Maulana <alfianmna@gmail.com>
2023-07-02 13:26:07 +0700

    first commit

---
+++ shsh.js
@@ -0,0 +1,13 @@
+function fetchUsername() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("JSUser");
+    }, 3000);
+  });
+}
+
+console.log("Fetching username...");
+fetchUsername().then((value) => {
+  console.log(`You are logged in as ${value}`);
+});
+console.log("Welcome!");
